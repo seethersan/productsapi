24c52f718357b8b5417588d1ed93c3c79c20422c
seethersan <carlos_jcez@hotmail.com>
2020-07-12 05:52:42 -0500

    [feature] added Products view with get method

--- products/views.py
+++ products/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views.generic import View
+from django.http import JsonResponse
+from products.models import Product
 
 # Create your views here.
+class Products(View):
+    def get(self, request, *args, **kwargs):
+        products = Product.objects.all()
+        return JsonResponse({"products": list(products.values())}, safe=False)
