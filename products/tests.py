494bbde09ea36ce0d4e1398bac08b38b3b07bee3
seethersan <carlos_jcez@hotmail.com>
2020-07-12 09:31:11 -0500

    added ProductsView post method tests

--- products/tests.py
+++ products/tests.py
@@ -3,7 +3,9 @@ from django.test import Client
 from django.core.validators import ValidationError
 
 from products.models import Product
-from products.views import Products
+from products.views import Products, ProductsInsert
+
+import json
 
 # Create your tests here.
 class ProductTest(TestCase):
@@ -57,3 +59,115 @@ class ProductView(TestCase):
         c = Client()
         response = c.get('/api/products/')
         self.assertEqual(response.status_code, 200)
+
+    def test_product_get_content(self):
+        c = Client()
+        products = [
+            	{
+                    "id": "product",
+                    "name": "awesome product",
+                    "value": 29.3,
+                    "discount_value": 3.2,
+                    "stock": 3
+                },
+                {
+                    "id": "product2",
+                    "name": "awesome product2",
+                    "value": 291.3,
+                    "discount_value": 33.2,
+                    "stock": 3
+                }
+        ]
+        create = c.post('/api/products/bulk_insert', json.dumps({'products': products}),
+                        content_type="application/json")
+        response = c.get('/api/products/')
+        response_products = json.loads(response.content)
+        self.assertEqual(products, response_products["products"])
+
+    def test_product_post_success_response(self):
+        c = Client()
+        products = [
+            	{
+                    "id": "product3",
+                    "name": "awesome product",
+                    "value": 29.3,
+                    "discount_value": 3.2,
+                    "stock": 3
+                },
+                {
+                    "id": "product4",
+                    "name": "awesome product2",
+                    "value": 291.3,
+                    "discount_value": 33.2,
+                    "stock": 3
+                }
+        ]
+        response = c.post('/api/products/bulk_insert', json.dumps({'products': products}),
+                        content_type="application/json")
+        self.assertEqual(response.status_code, 200)
+
+    def test_product_post_fail_response(self):
+        c = Client()
+        products = [
+            	{
+                    "id": "product5",
+                    "name": "aw",
+                    "value": 29.3,
+                    "discount_value": 3.2,
+                    "stock": -3
+                },
+                {
+                    "id": "product6",
+                    "name": "awesome product2",
+                    "value": 291.3,
+                    "discount_value": 333.2,
+                    "stock": 3
+                }
+        ]
+        response = c.post('/api/products/bulk_insert', json.dumps({'products': products}),
+                    content_type="application/json")
+        self.assertEqual(response.status_code, 422)
+
+    def test_product_post_fail_validators(self):
+        c = Client()
+        products = [
+            	{
+                    "id": "product7",
+                    "name": "aw",
+                    "value": 0,
+                    "discount_value": 32.2,
+                    "stock": -3
+                }
+        ]
+        response = c.post('/api/products/bulk_insert', json.dumps({'products': products}),
+                    content_type="application/json")
+        errors = json.loads(response.content)["products_report"][0]["errors"]
+        self.assertTrue("Invalid product name" in errors)
+        self.assertTrue("Invalid value" in errors)
+        self.assertTrue("Invalid discount value" in errors)
+        self.assertTrue("Invalid stock value" in errors)
+
+    def test_product_post_fail_parse(self):
+        c = Client()
+        products = [
+            	{
+                    "ide": "product"
+                },
+                {
+                    "names": "products"
+                },
+                {
+                    "values": "323"
+                },
+                {
+                    "id": "product",
+                    "name": "awesome product",
+                    "value": "asf",
+                    "discount_value": 32.2,
+                    "stock": -3
+                }
+        ]
+        response = c.post('/api/products/bulk_insert', json.dumps({'products': products}),
+                    content_type="application/json")
+        error = json.loads(response.content)
+        self.assertEqual(4, error["number_of_products_unable_to_parse"])
