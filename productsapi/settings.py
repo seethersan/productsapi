60d9f78caf066c12ab7f414db3c80fd474690425
seethersan <carlos_jcez@hotmail.com>
2020-07-13 02:05:54 -0500

    [fix] changed Debug to False in settings

--- productsapi/settings.py
+++ productsapi/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '8z4ca&cxh2oyrm#3h9z_ikboy8u&qb-*gndg9l!i)d2xua5@v@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
