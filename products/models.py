1a95da16d8d759fc5f53d2d72a419ed15fae8914
seethersan <carlos_jcez@hotmail.com>
2020-07-21 16:14:01 -0500

    [feature] added Category class

--- products/models.py
+++ products/models.py
@@ -10,11 +10,20 @@ def validate_stock(value):
         raise ValidationError("Invalid stock value")
 
 # Create your models here.
+class Category(models.Model):
+    id = models.CharField(max_length=10, primary_key=True)
+    name = models.CharField(max_length=100, validators=[MinLengthValidator(3, "Invalid product name"), MaxLengthValidator(55, "Invalid product name")])
+    description = models.CharField(max_length=250, validators=[MinLengthValidator(3, "Invalid product name"), MaxLengthValidator(250, "Invalid product name")])
+
+    def __str__(self):
+        return self.name
+
 class Product(models.Model):
     id = models.CharField(max_length=10, primary_key=True)
     name = models.CharField(max_length=100, validators=[MinLengthValidator(3, "Invalid product name"), MaxLengthValidator(55, "Invalid product name")])
     value = models.FloatField(validators=[validate_value])
     discount_value = models.FloatField()
+    categories = models.ManyToManyField(Category)
     stock = models.IntegerField(validators=[validate_stock])
 
     def __str__(self):
