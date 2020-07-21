3a9f8168a26235281a8a151d074c6cf9eaf5eabd
seethersan <carlos_jcez@hotmail.com>
2020-07-21 16:14:27 -0500

    [feature] added CategorySerializer

--- products/serializers.py
+++ products/serializers.py
@@ -1,7 +1,14 @@
 from rest_framework import serializers
-from .models import Product
+from .models import Product, Category
+
+class CategorySerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Category
+        fields = ('id', 'name', 'description')
 
 class ProductSerializer(serializers.HyperlinkedModelSerializer):
+    categories = CategorySerializer(many=True, read_only=True)
     class Meta:
         model = Product
-        fields = ('id', 'name', 'value', 'discount_value', 'stock')
+        fields = ('id', 'name', 'value', 'discount_value', 'stock', 'categories')
+        extra_kwargs = {'categories': {'required': False}}
