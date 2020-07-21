032732d83094eac95625a1b6148c2c23ebb4a358
seethersan <carlos_jcez@hotmail.com>
2020-07-21 18:38:51 -0500

    [fix] refactoring Books and Products Serializers

--- products/serializers.py
+++ products/serializers.py
@@ -1,14 +1,16 @@
 from rest_framework import serializers
 from .models import Product, Category
 
-class CategorySerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = Category
-        fields = ('id', 'name', 'description')
-
-class ProductSerializer(serializers.HyperlinkedModelSerializer):
-    categories = CategorySerializer(many=True, read_only=True)
+class ProductSerializer(serializers.ModelSerializer):
+    categories = serializers.PrimaryKeyRelatedField(queryset=Category.objects.all(), many=True)
     class Meta:
         model = Product
         fields = ('id', 'name', 'value', 'discount_value', 'stock', 'categories')
         extra_kwargs = {'categories': {'required': False}}
+
+class CategorySerializer(serializers.ModelSerializer):
+    products = ProductSerializer(many=True, read_only=True)
+    class Meta:
+        model = Category
+        fields = ('id', 'name', 'description', 'products')
+
